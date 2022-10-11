8d75ca8d5f86bee433599ef58956d699ea59a12b
roey <roeylalazar@gmail.com>
2022-10-12 01:10:58 +0300

    plugin ready

--- pkg/testrunner/framework/pytest/plugin.py
+++ pkg/testrunner/framework/pytest/plugin.py
@@ -1,14 +1,14 @@
-import sys
 import contextlib
 import json
+import sys
 import time
 from collections import defaultdict
 from pathlib import Path
-from typing import Optional
-from pydantic import BaseModel
+from typing import List, Optional
 
 import pytest
 from coverage import CoverageData
+from pydantic import BaseModel
 from pytest_jsonreport.plugin import JSONReport
 
 
@@ -26,6 +26,15 @@ class Scope(HashableBaseModel):
     endline: int = None
     endcol: int = None
 
+
+class TestResult(HashableBaseModel):
+    call_graph: List[Scope]
+    test_func_scope: Optional[Scope]
+    name: str
+    success: bool
+    time_in_ms: float
+
+
 class TestSelectionPlugin:
     def __init__(self, tests_to_skip):
         self.tests_to_skip = tests_to_skip
@@ -37,42 +46,46 @@ class TestSelectionPlugin:
         for test in items:
             test_to_skip = self.tests_to_skip.get(test.nodeid)
             if test_to_skip != None:
-                if test_to_skip == True: # passed == true
+                if test_to_skip == True:  # passed == true
                     test.add_marker(skip_mark)
                 else:
                     test.add_marker(xfail_mark)
 
+
 def run_tests(tests_to_skip, args):
-        json_report = JSONReport()
+    json_report = JSONReport()
 
-        run_tests_start_time = time.time()
-        status_code = pytest.main(
-            args,
-            plugins=[TestSelectionPlugin(tests_to_skip), json_report],
-        )
-        run_tests_time = time.time() - run_tests_start_time
-        coverage_data = _get_coverage_data()
+    run_tests_start_time = time.time()
+    status_code = pytest.main(
+        args,
+        plugins=[TestSelectionPlugin(tests_to_skip), json_report],
+    )
+    run_tests_time = time.time() - run_tests_start_time
+    coverage_data = _get_coverage_data()
 
-        ran_tests = [
-            {
-                "name": test["nodeid"],
-                "success": is_passed(test["outcome"]),
-                "time_in_ms": calculate_test_time(test),
-                "call_graph": list(coverage_data.get(test["nodeid"], set({})))
-            } for test in json_report.report["tests"] if not is_skipped(test["outcome"])
-        ]
-        return ran_tests, status_code
+    ran_tests = [
+        TestResult(name=test["nodeid"],
+                   success=is_passed(test["outcome"]),
+                   time_in_ms=calculate_test_time(test),
+                   call_graph=list(coverage_data.get(test["nodeid"], set({}))))
+        for test in json_report.report["tests"] if not is_skipped(test["outcome"])
+    ]
+    return ran_tests, status_code
+
 
 def calculate_test_time(test: dict):
     return float(test.get('duration', 0) +
-                    test['teardown'].get('duration', 0) +
-                    test['setup'].get('duration', 0)) * 1000
+                 test['teardown'].get('duration', 0) +
+                 test['setup'].get('duration', 0)) * 1000
+
 
 def is_skipped(test_outcome: str):
     return test_outcome == "skipped" or test_outcome == "xfailed"
 
+
 def is_passed(test_outcome: str):
     return test_outcome == "passed"
+
 
 def _get_coverage_data():
     cov = CoverageData()
@@ -91,20 +104,32 @@ def _get_coverage_data():
                         endline=lineno
                     ))
 
-
     return data
+
 
 def to_project(repo: Path, measured_file: Path) -> Optional[Path]:
     with contextlib.suppress(ValueError):
         return measured_file.relative_to(repo.resolve())
 
-def main(tests_to_skip, args):
-    args = ["-v", "--cov", "--cov-context=test", "--json-report-file=none"] + args
+
+def main(tests_to_skip, result_path, args):
+    args = ["-v", "--cov", "--cov-context=test",
+            "--json-report-file=none"] + args
     tests, exit_code = run_tests(tests_to_skip, args)
+    # dump to json with pydantic
+    tests_dict = {}
+    with open(result_path, "w") as f:
+        for test in tests:
+            tests_dict[test.name] = test.dict()
+        json.dump(tests_dict, f)
+
     return exit_code
 
+
 if __name__ == "__main__":
-    _ = sys.argv[0]                             # plugin.py
-    tests_to_skip = json.loads(sys.argv[1])     # "{"test_file1.py:test1": true, "test_file1.py:test2": false, "test_file2.py:test3": true}" {TEST_NAME: PASSED}
-    args = sys.argv[2:]                         # "-v", "--cov", "--cov-context=test", "--json-report-file=none"
-    sys.exit(main(tests_to_skip, sys.argv[2:]))
+    _ = sys.argv[0]  # plugin.py
+    # "{"test_file1.py:test1": true, "test_file1.py:test2": false, "test_file2.py:test3": true}" {TEST_NAME: PASSED}
+    result_path = sys.argv[1]  # "/tmp/pytest-result.json"
+    tests_to_skip = json.loads(sys.argv[2])
+    args = sys.argv[3:]     # "-v", "--cov", "--cov-context=test", "--json-report-file=none"
+    sys.exit(main(tests_to_skip, result_path, args))
